6b0b6d5adc27831cf72feb40fee74ec1980ff439
Marx-wrld <iongonga@kabarak.ac.ke>
2023-10-05 11:26:05 +0300

    Adding the hands animation and removing it after playing

--- app.js
+++ app.js
@@ -16,16 +16,21 @@ const game = () => {
 
     //Playing the Match
     const playMatch = () => {
-      const options = document.querySelectorAll(".options button");
-      const playerHand = document.querySelector(".player-hand");
-      const computerHand = document.querySelector(".computer-hand");
-      const hands = document.querySelectorAll(".hands img");
+
+      const options = document.querySelectorAll(".options button"); //selects all the buttons in the options class
+
+      const playerHand = document.querySelector(".player-hand"); //selects the player hand
+
+      const computerHand = document.querySelector(".computer-hand"); //selects the computer hand
+
+      const hands = document.querySelectorAll(".hands img"); //selects all the images in the hands class
   
-      hands.forEach(hand => {
+      hands.forEach(hand => { //loops through all the images in the hands class
         hand.addEventListener("animationend", function() {
-          this.style.animation = "";
+          this.style.animation = ""; //removes the animation after it is done playing
         });
       });
+
       //Computer Options
       const computerOptions = ["rock", "paper", "scissors"];
   
@@ -36,7 +41,7 @@ const game = () => {
           const computerNumber = Math.floor(Math.random() * 3); //random number between 0 and 2
           const computerChoice = computerOptions[computerNumber]; //randomly chooses rock, paper, or scissors
   
-          setTimeout(() => {
+          setTimeout(() => { //sets a timer of 2 seconds
 
             //Calling compare hands
             compareHands(this.textContent, computerChoice);
@@ -47,12 +52,12 @@ const game = () => {
             computerHand.src = `./assets/${computerChoice}.png`; //computerChoice is the random choice of the computer
           }, 2000);
 
-    //       //Animation
-    //       playerHand.style.animation = "shakePlayer 2s ease";
-    //       computerHand.style.animation = "shakeComputer 2s ease";
-    //     });
-    //   });
-    // };
+          //Hands animation
+          playerHand.style.animation = "shakePlayer 2s ease";
+          computerHand.style.animation = "shakeComputer 2s ease";
+        });
+      });
+    };
   
     const updateScore = () => { //updates the score
 
